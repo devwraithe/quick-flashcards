16576e14048191a2375acfb907d3b158b769ddb6
ibrahim-ibrahim-dsa <folahanmi0001@gmail.com>
2023-07-08 18:42:55 +0100

    Created new files for CustomIcon and CustomSubIcon widgets for controlling the stacked cards

--- lib/app/presentation/screens/home_screen.dart
+++ lib/app/presentation/screens/home_screen.dart
@@ -5,16 +5,13 @@ import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_flashcards/app/core/theme/app_colors.dart';
 import 'package:quick_flashcards/app/core/theme/text_theme.dart';
-import 'package:quick_flashcards/app/presentation/logic/flashcards_logic/add_flashcard_notifier.dart';
 import 'package:quick_flashcards/app/presentation/logic/flashcards_logic/get_flashcards_notifier.dart';
 import 'package:quick_flashcards/app/presentation/widgets/back_flashcard.dart';
-import 'package:quick_flashcards/app/presentation/widgets/card_color_picker.dart';
+import 'package:quick_flashcards/app/presentation/widgets/custom_sub_icon.dart';
 import 'package:quick_flashcards/app/presentation/widgets/front_flashcard.dart';
 
-import '../../core/constants/string_constants.dart';
 import '../../core/helpers/ui_helper.dart';
-import '../../core/helpers/validators_helper.dart';
-import '../widgets/app_textfield_widget.dart';
+import '../widgets/custom_icon.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -65,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     GestureDetector(
-                      onTap: () => addFlashcard(),
+                      // onTap: () => addFlashcard(), // navigate to the add_flashcard screen
                       child: const Icon(
                         Icons.add,
                         size: 28,
@@ -75,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
               ),
-              const SizedBox(height: 36),
+              const SizedBox(height: 34),
               Consumer(
                 builder: (context, ref, _) {
                   final flashcard = ref.watch(getFlashcardProv);
@@ -89,6 +86,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           allowedSwipeDirection:
                               AllowedSwipeDirection.symmetric(
                             horizontal: true,
+                            vertical: true,
                           ),
                           numberOfCardsDisplayed: 3,
                           scale: 0.96,
@@ -137,159 +135,34 @@ class _HomeScreenState extends State<HomeScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  subIcon(Icons.arrow_back_rounded, () {
-                    _swiperController.swipeLeft();
-                  }),
-                  icon(Icons.close, AppColors.red, () {
-                    _swiperController.swipeLeft();
-                  }),
-                  subIcon(Icons.flip_rounded, () {
-                    _flipController.toggleCard();
-                  }),
-                  icon(Icons.check, AppColors.green, () {
-                    _swiperController.swipeRight();
-                  }),
-                  subIcon(Icons.arrow_forward_rounded, () {
-                    _swiperController.swipeRight();
-                  }),
+                  CustomSubIcon(
+                    icon: Icons.arrow_back_rounded,
+                    onTap: () => _swiperController.swipeLeft(),
+                  ),
+                  CustomIcon(
+                    icon: Icons.close,
+                    color: AppColors.red,
+                    onTap: () => _swiperController.swipeLeft(),
+                  ),
+                  CustomSubIcon(
+                    icon: Icons.flip_rounded,
+                    onTap: () => _flipController.toggleCard(),
+                  ),
+                  CustomIcon(
+                    icon: Icons.check,
+                    color: AppColors.green,
+                    onTap: () => _swiperController.swipeRight(),
+                  ),
+                  CustomSubIcon(
+                    icon: Icons.arrow_forward_rounded,
+                    onTap: () => _swiperController.swipeRight(),
+                  ),
                 ],
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget icon(IconData icon, Color iconColor, void Function()? onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        height: 58,
-        width: 58,
-        child: Icon(
-          icon,
-          size: 28,
-          color: iconColor,
-        ),
-      ),
-    );
-  }
-
-  Widget subIcon(IconData icon, void Function()? onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.darkGray,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        height: 50,
-        width: 50,
-        child: Icon(
-          icon,
-          size: 22,
-          color: AppColors.white,
-        ),
-      ),
-    );
-  }
-
-  final _questionController = TextEditingController();
-  final _answerController = TextEditingController();
-
-  String? _selectedCardColor;
-
-  final _key = GlobalKey<FormState>(debugLabel: 'add_flashcard');
-
-  _addFlashcard(context, ref) async {
-    final formState = _key.currentState!;
-
-    final state = ref.watch(fcProvider);
-    final notifier = ref.watch(fcProvider.notifier);
-
-    try {
-      if (formState.validate()) {
-        formState.save();
-        final result = await notifier.addFlashcard(
-          _questionController.text,
-          _answerController.text,
-          _selectedCardColor ?? AppColors.cardGreen.toString(),
-        );
-        if (state != AddFlashcardState.success) {
-          debugPrint('[UI AUTH ERROR] $result');
-          // return AppSnackbar.error(context, result);
-        } else {
-          // Navigator.pushNamed(context, Routes.home);
-        }
-      }
-    } catch (e) {
-      debugPrint("${StringConstants.unknownError}: ${e.toString()}");
-    }
-  }
-
-  // dialog box to add flashcard
-  addFlashcard() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog.fullscreen(
-          child: Form(
-            key: _key,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text("Add a Flashcard"),
-                AppTextFieldWidget(
-                  hintText: "Question",
-                  controller: _questionController,
-                  validator: (v) => ValidatorsHelper.def(v),
-                  onSaved: (v) => _questionController.text = v!,
-                ),
-                AppTextFieldWidget(
-                  hintText: "Answer",
-                  controller: _answerController,
-                  validator: (v) => ValidatorsHelper.def(v),
-                  onSaved: (v) => _answerController.text = v!,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    for (final cardColor in UiHelpers.cardColors)
-                      CardColorPicker(
-                        onTap: () {
-                          _selectedCardColor = cardColor.toString();
-                        },
-                        cardColor: cardColor,
-                        borderColor: _selectedCardColor == cardColor.toString()
-                            ? Colors.black45
-                            : Colors.transparent,
-                      ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Consumer(
-                  builder: (context, ref, _) {
-                    final state = ref.watch(fcProvider);
-
-                    return FilledButton(
-                      onPressed: () => _addFlashcard(context, ref),
-                      child: state == AddFlashcardState.loading
-                          ? UiHelpers.loader()
-                          : const Text("Add Flashcard"),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }

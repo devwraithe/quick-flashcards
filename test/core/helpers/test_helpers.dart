9c17807f4c79b0ff09cf5e030202f87566b349f1
ibrahim-ibrahim-dsa <folahanmi0001@gmail.com>
2023-09-19 22:45:51 +0100

    fix: remove the test folder

--- test/core/helpers/test_helpers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:mockito/annotations.dart';
-import 'package:quick_flashcards/app/presentation/providers/flashcards_logic/add_flashcard_notifier.dart';
-
-@GenerateMocks([
-  AddFlashcardNotifier,
-  FirebaseAppPlatform,
-])
-void main() {}
